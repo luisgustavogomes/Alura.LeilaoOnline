b81421ce54ce5a7d2edbd84a873ea1fe34769f1c
luis gomes <luisgustavogomes@outlook.com>
2019-05-16 12:41:10 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.LeilaoOnline/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alura.LeilaoOnline
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
