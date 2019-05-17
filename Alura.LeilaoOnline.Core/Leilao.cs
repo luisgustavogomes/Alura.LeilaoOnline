a36be0f7670c18c6f3fadd7a011b386994cd85de
luis gomes <luisgustavogomes@outlook.com>
2019-05-17 07:56:59 -0300

    Iniciando

--- Alura.LeilaoOnline.Core/Leilao.cs
+++ Alura.LeilaoOnline.Core/Leilao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Alura.LeilaoOnline.Core
@@ -9,6 +10,8 @@ namespace Alura.LeilaoOnline.Core
         private IList<Lance> _lances;
         public IEnumerable<Lance> Lances => _lances;
         public string Peca { get; }
+        public Lance Ganhador { get; private set; }
+
 
         public Leilao(string peca)
         {
@@ -28,7 +31,7 @@ namespace Alura.LeilaoOnline.Core
 
         public void TerminaPregao()
         {
-
+            Ganhador = Lances.Last();
         }
 
     }
